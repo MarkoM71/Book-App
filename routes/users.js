e107d73220145e18c9b0bfe664373aa229fc2af1
Marko Matura <Marko.Matura@email.com>
2024-01-06 13:11:44 -0500

    Update routes for login

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ router.post('/', createUser);
 router.post('/login', loginUser)
 
 //Route to Get all users
+
 router.get('/', getAllUsers);
 
 //Route to Get One User //Passing the user ID in the URL parameter.
